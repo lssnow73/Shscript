e432cf749fb307302e9b9877eabeaed061b8bc22
lssnow <seoungsoo.lee@atto-research.com>
2022-12-06 13:12:46 +0900

    Add ZTP post procedure(ztp_post_proc.sh) And Change file mode

--- ZTPrun.sh
+++ ZTPrun.sh
@@ -3,9 +3,59 @@
 # Be careful! Only if "/usr/local/openvswitch" exists, Run post_init_hook.sh 
 
 #echo "Starting ZTPrun.sh"
+if [ -z $ZTP_POS ]; then
+    ZTP_POS=`pwd`
+    export ZTP_POS
+fi
+echo "ZTPrun.sh" $ZTP_POS
+CONN4NET_FILE=$ZTP_POS/conn4net.conf
+export CONN4NET_FILE
+
+SYSID=`cat /tmp/oem_info | grep hardware_type | awk -F'[ -]' '{print $NF}'`
+if [ $SYSID == "BP" ]; then
+    SYSID=`cat /tmp/oem_info | grep hardware_type | awk -F'[ -]' '{print $(NF-1)}'`
+fi
+echo $SYSID
+
+case $SYSID in
+    8T4X)
+        echo "SYSTEM is $SYSID. ZTP Port is eth-0-1"
+        ZTP_PORT="0-1"
+        ;;
+    24P4XJ)
+        echo "SYSTEM is $SYSID. ZTP Port is eth-0-25"
+        ZTP_PORT="0-25"
+        ;;
+    48HP)
+        echo "SYSTEM is $SYSID. ZTP Port is eth-0-49"
+        ZTP_PORT="0-49"
+        ;;
+    24T4X)
+        echo "SYSTEM is $SYSID. ZTP Port is eth-0-25"
+        ZTP_PORT="0-25"
+        ;;
+    48X6Q | SFC9500A)
+        echo "SYSTEM is $SYSID."
+        cp -f $ZTP_POS/startup-config.conf /mnt/flash/startup-config.conf
+        touch $ZTP_POS/ztp_complete
+        sync
+        sleep 3
+        sync
+        reboot -f
+        ;;
+    *)
+        echo "SYSTEM is unknown. ZTP Port is eth-0-1"
+        ZTP_PORT="0-1"
+        ;;
+esac
+
+echo "ZTP_PORT is eth-$ZTP_PORT"
+export ZTP_PORT
+
+sed -in "s/\(monitor .* source .* eth-\)\(.*\)/\1$ZTP_PORT rx/" $ZTP_POS/ZTPrun.conf
 
 # Make settings for connecting to the network
-/usr/sbin/klish -q -s /mnt/udisk/ZTPrun.conf
+/usr/sbin/klish -q -s $ZTP_POS/ZTPrun.conf
 
 # Wait rpc-api about 15 seconds
 for i in $(seq 1 10); do
@@ -17,10 +67,10 @@ echo ""
 
 # Clear cpu packet buffer
 rm -rf /mnt/flash/mirror/*
-/mnt/udisk/CPUpktCapture.py clear
+$ZTP_POS/CPUpktCapture.py clear
 
 # Make packet capture file under /mnt/flash/mirror/
-/mnt/udisk/CPUpktCapture.py start
+$ZTP_POS/CPUpktCapture.py start
 
 # Capture packet about 30 seconds
 #for i in {1..30} Not work in sh without bash
@@ -32,17 +82,23 @@ done
 echo ""
 
 # Stop packet capture file under /mnt/flash/mirror/
-/mnt/udisk/CPUpktCapture.py stop
+$ZTP_POS/CPUpktCapture.py stop
 
 # packet parser and make configure
-/mnt/udisk/pktParserAndMakeConfig.sh
+$ZTP_POS/pktParserAndMakeConfig.sh
+RESULT=$?
+echo "pktParse result is $RESULT"
 sync
 sync
 sleep 3
 
 #reboot -f
 
-/usr/sbin/klish -q -s /mnt/udisk/conn4net.conf
+if [ -f $CONN4NET_FILE ]; then
+    /usr/sbin/klish -q -s $CONN4NET_FILE
+else
+    /usr/sbin/klish -q -s /mnt/flash/boot/.factory-config.conf
+fi
 
 echo "End ZTP"
 echo "End post INIT"
--- post_init_hook.sh
+++ post_init_hook.sh
@@ -4,6 +4,8 @@
 echo "Start post INIT ...."
 
 USB_MOUNT_DIR="/mnt/udisk"
+ZTP_DIR="/mnt/flash/ztp"
+ZTP_POS=""
 isUSB="false"
 isZTP="false"
 isDEFAULTCONF="false"
@@ -11,33 +13,51 @@ ZTPFILE="$USB_MOUNT_DIR/ZTPrun.sh"
 STARTUP_CONFIG="/mnt/flash/startup-config.conf"
 FACTORY_CONFIG="/mnt/flash/boot/.factory-config.conf"
 
-# /mnt/flash/startup-config.conf file exists at this point
-# Compare /mnt/flash/startup-config.conf and /mnt/flash/boot/.factory-config.conf 
-diff $STARTUP_CONFIG $FACTORY_CONFIG > /dev/null 2>&1
-isDEFAULTCONF=`[ $? -eq 0 ] && echo "true" || echo "false"`
-if [ $isDEFAULTCONF == "false" ]; then
-    echo "End post INIT"
-    exit 0
-fi
-
 # Check mount USB Drive
 echo -n "USB Drive "
 
 if [ -d $USB_MOUNT_DIR ]; then
     echo "detected"
     isUSB="true"
+    ZTP_POS=$USB_MOUNT_DIR
 else
     echo "not detected"
-fi
-
-# Check ZTP script
-if [ $isUSB == "true" ]; then
-    if [ -f "$ZTPFILE" ]; then
-        isZTP="true"
+    isUSB="false"
+    if [ -d $ZTP_DIR ]; then
+        ZTP_POS=$ZTP_DIR
     fi
 fi
 
-#echo "USB is $isUSB, ZTP is $isZTP"
+export ZTP_POS
+echo $ZTP_POS
+ZTPFILE="$ZTP_POS/ZTPrun.sh"
+
+
+# /mnt/flash/startup-config.conf file exists at this point
+# Compare /mnt/flash/startup-config.conf and /mnt/flash/boot/.factory-config.conf 
+if [ -f $STARTUP_CONFIG ]; then
+    diff $STARTUP_CONFIG $FACTORY_CONFIG > /dev/null 2>&1
+    isDEFAULTCONF=`[ $? -eq 0 ] && echo "true" || echo "false"`
+else
+    isDEFAULTCONF="true"
+fi
+if [ $isDEFAULTCONF == "false" ]; then
+    if [ -f $ZTP_POS/ztp_complete ]; then
+        rm -f $ZTP_POS/ztp_complete
+        if [ -f $ZTP_POS/ztp_post_proc.sh ]; then
+            $ZTP_POS/ztp_post_proc.sh
+        fi
+    fi
+    echo "End post INIT"
+    exit 0
+fi
+
+# Check ZTP script
+if [ -f "$ZTPFILE" ]; then
+    isZTP="true"
+fi
+
+echo "USB is $isUSB, ZTP is $isZTP"
 
 # If ZTP script, run it
 if [ $isDEFAULTCONF == "true" ] && [ $isZTP == "true" ]; then
